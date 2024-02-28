533f50be9cad61d7a49932f2c607f6608507e2a7
Roy <Roy.Rotstein87@gmail.com>
2024-02-27 19:08:00 -0800

    blinks

---
+++ blink-led/README.md
@@ -0,0 +1,7 @@
+This code is cross-compiled to a Raspberry Pi 4 B device running Ubuntu.
+Install cargo-cross.
+Set the target as follows:
+
+```bash
+cross build --target aarch64-unknown-linux-gnu
+```
---
+++ blink-led/src/main.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+
+const GPIO_LED: u8 = 17;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+
+    loop {
+        println!("Enter number of blinks: ");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input: u32 = input.trim().parse().unwrap();
+        for _ in 0..input {
+            pin.set_high();
+            thread::sleep(Duration::from_millis(500));
+            pin.set_low();
+            thread::sleep(Duration::from_millis(500));
+        }
+    }
+}
