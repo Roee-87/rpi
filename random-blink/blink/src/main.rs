3a643900294c3e146e0a603eb97ed2254f059ef4
Roy <Roy.Rotstein87@gmail.com>
2024-02-28 19:16:47 -0800

    updated PIPE paths

--- random-blink/blink/src/main.rs
+++ random-blink/blink/src/main.rs
@@ -1,33 +1,24 @@
-use std::io::Write;
-use std::process::{Command, Stdio};
-use std::thread;
-use std::time::Duration;
-use std::error::Error;
-
 #[cfg(target_os = "linux")]
 use rppal::gpio::Gpio;
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
+use std::thread;
+use std::time::Duration;
 
 #[cfg(target_os = "linux")]
-const PIPE_1: &`static str = "./pipe1";
+const PIPE_1: &'static str = "./pipe1";
 
 #[cfg(not(target_os = "linux"))]
-const PIPE_1: &`static str = "../pipe1";
+const PIPE_1: &'static str = "../pipe1";
 
 #[cfg(target_os = "linux")]
-const PIPE_2: &`static str = "./pipe2";
+const PIPE_2: &'static str = "./pipe2";
 
 #[cfg(not(target_os = "linux"))]
-const PIPE_2: &`static str = "../pipe2";
+const PIPE_2: &'static str = "../pipe2";
 
-#[cfg(target_os = "linux")]
-const PATH_RNG: &'static str = "./random";
-
-#[cfg(not(target_os = "linux"))]
-const PATH_RNG: &'static str = "../random/target/debug/random";
-
-#[cfg(target_os = "linux")]
 const GPIO_LED: u8 = 17;
-
 // blink time in milliseconds
 const BLINK_TIME: u64 = 250;
 
@@ -38,38 +29,36 @@ fn main() -> Result<(), Box<dyn Error>> {
         #[cfg(target_os = "linux")]
         pin.set_high();
         print!("Enter a command: blink or exit\n");
-
         let mut input = String::new();
         std::io::stdin()
             .read_line(&mut input)
             .expect("Failed to read from stdin");
         let input = input.trim();
-
         match input {
             "blink" => {
-                // Spawn the second program
-                let mut child = Command::new(PATH_RNG)
-                    .stdin(Stdio::piped())
-                    .stdout(Stdio::piped())
-                    .spawn().expect("Failed to spawn program-2");
-                
-                println!("making the request to program-2...");
-                // Communicate with the second program
-                if let Some(mut stdin) = child.stdin.take() {
-                    // Send a request for a random number
-                    writeln!(stdin, "generate_random_number").expect("Failed to write to stdin");
-                }
+                let mut pipe_writer =
+                    File::create(PIPE_1).expect("Failed to open the named pipe for writing");
 
-                println!("request sent");
+                // Send a request for a random number directly to program2
+                writeln!(pipe_writer, "generate_random_number")
+                    .expect("Failed to write to the named pipe");
 
-                // Wait for the second program to finish
-                let output = child
-                    .wait_with_output()
-                    .expect("Failed to wait for program-2");
+                print!("Request sent\n");
+                io::stdout().flush().expect("Failed to flush stdout");
 
-                let value = String::from_utf8_lossy(&output.stdout);
+                pipe_writer.flush().expect("Failed to flush the named pipe");
 
-                let blinks = value.trim().parse::<i32>()?;
+                // Open the named pipe for reading
+                let mut pipe_reader =
+                    File::open(PIPE_2)).expect("Failed to open the named pipe for reading");
+                let mut reader = BufReader::new(pipe_reader);
+
+                // Read the response from program2
+                let mut response = String::new();
+                reader
+                    .read_line(&mut response)
+                    .expect("Failed to read from the named pipe");
+                let blinks = response.trim().parse::<i32>().unwrap();
                 #[cfg(target_os = "linux")]
                 for _ in 0..blinks {
                     pin.set_low();
@@ -78,8 +67,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                     thread::sleep(Duration::from_millis(BLINK_TIME));
                 }
 
-                // Print the result from the second program
-                println!("Program-1 received: {:?}", blinks);
+                print!("Program 1 received: {}\n", &blinks);
+                io::stdout().flush().expect("Failed to flush stdout");
             }
             "exit" => {
                 break;
