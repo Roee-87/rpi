310af6746d221431d0124b092df1afab23a4ae77
Roy <Roy.Rotstein87@gmail.com>
2024-03-08 17:13:30 -0800

    added ctrl+c exit with display turning off

--- touchpad/src/main.rs
+++ touchpad/src/main.rs
@@ -7,13 +7,14 @@ use rppal::gpio::{Gpio, InputPin, OutputPin};
 
 const ROW_PINS: [u8; 4] = [18, 23, 24, 25];
 const COL_PINS: [u8; 4] = [10, 22, 27, 17];
-const keys: [char; 16] = [
+const KEYS: [char; 16] = [
     '1', '2', '3', 'A', '4', '5', '6', 'B', '7', '8', '9', 'C', '*', '0', '#', 'D',
 ];
 
 struct Keypad {
     rows: Vec<OutputPin>,
     cols: Vec<InputPin>,
+    keys: Vec<char>,
 }
 
 impl Keypad {
@@ -24,27 +25,50 @@ impl Keypad {
 
         for i in 0..4 {
             rows.push(gpio.get(ROW_PINS[i])?.into_output());
-            cols.push(gpio.get(COL_PINS[i])?.into_input());
+            cols.push(gpio.get(COL_PINS[i])?.into_input_pulldown());
         }
 
-        Ok(Keypad { rows, cols })
+        let keys: Vec<char> = Vec::from(KEYS);
+
+        Ok(Keypad { rows, cols, keys })
     }
 
-    fn read(&mut self) -> Option<char> {
+    fn read(&mut self) -> Option<Vec<char>> {
+        let mut pressed_keys: Vec<char> = Vec::new();
         for i in 0..4 {
             self.rows[i].set_high();
             for j in 0..4 {
                 if self.cols[j].is_low() {
-                    return Some(keys[i * 4 + j]);
+                    pressed_keys.push(self.keys[i * 4 + j]);
                 }
             }
             self.rows[i].set_low();
         }
-        None
+        Some(pressed_keys)
     }
 
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn Error>>{
+    let mut last_key_pressed = Vec::new();
+    let mut keypad = Keypad::new()?;
+
+    // Set up a handler for Ctrl-C to exit the program
+    let running = std::sync::Arc::new(std::sync::atomic::AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, std::sync::atomic::Ordering::SeqCst);
+    })?;
+
+    
+    while running.load(std::sync::atomic::Ordering::SeqCst) {
+        let pressed_keys = keypad.read().ok_or("Error reading keypad")?;
+        if pressed_keys != last_key_pressed && !pressed_keys.is_empty() {
+            println!("{:?}", pressed_keys);
+        }
+        last_key_pressed = pressed_keys;
+        
+        thread::sleep(Duration::from_millis(100));
+    }
+    Ok(())
 }
